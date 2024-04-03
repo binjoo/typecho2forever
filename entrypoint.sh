55b6b5ba03b06f9e08e18723d8fefdd2ffe774e9
liangj <icesword28@qq.com>
2024-04-03 21:36:16 +0800

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -eu
 export GITHUB="true"
 echo "Starting with args path: $path"
 echo "Starting with args url: $url"
-echo "Starting with args $username: $username"
+echo "Starting with args username: $username"
 echo "Starting with args password: $password"
 sh -c "java -jar /app.jar --app.path=$path --spring.datasource.url=$url --spring.datasource.username=$username --spring.datasource.password=$password"
