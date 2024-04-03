eeb82acccfbb96ce7e78087f89b786a7fd16bf29
liangj <icesword28@qq.com>
2024-04-03 21:59:29 +0800

    Update Application.java

--- src/main/java/com/github/binjoo/Application.java
+++ src/main/java/com/github/binjoo/Application.java
@@ -1,7 +1,6 @@
 package com.github.binjoo;
 
 import com.github.binjoo.app.*;
-import com.github.binjoo.forever.app.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
